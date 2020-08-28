dd862c7e8b0295069e0e3167311e55a9d54bad34
Luis Abellan <luis.abellan.dev@gmail.com>
2020-08-29 01:14:15 +0200

    added dft and bft and needed classes Queue and Stack

--- adv.py
+++ adv.py
@@ -12,9 +12,9 @@ world = World()
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
-# map_file = "maps/test_loop.txt"
+map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+# map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -27,7 +27,134 @@ player = Player(world.starting_room)
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
-traversal_path = []
+traversal_path = [] 
+
+class Stack():
+    def __init__(self):
+        self.stack = []
+    def push(self, value):
+        self.stack.append(value)
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+    def size(self):
+        return len(self.stack)
+
+
+def dft(self, starting_vertex):
+    """
+    Print each vertex in depth-first order
+    beginning from starting_vertex.
+    """
+    # Create an empty stack and add the starting_vertex
+    stack = Stack()
+    stack.push(starting_vertex) 
+    # Create an empty set to track visited verticies
+    visited_vertices = set()
+    # while the stack is not empty:
+    while stack.size() > 0:
+        # get current vertex (pop from stack)
+        current_vertex = stack.pop()
+        # Check if the current vertex has not been visited:
+        if current_vertex not in visited_vertices:
+            # print the current vertex
+            print(current_vertex)
+            # Mark the current vertex as visited
+            # Add the current vertex to a visited_set
+            visited_vertices.add(current_vertex)
+
+
+            # push up all the current vertex's neighbors (so we can visit them next)
+            for neighbor_vertex in self.get_neighbors(current_vertex):
+                stack.push(neighbor_vertex)
+
+
+        return None
+
+class Queue():
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.queue)
+        
+def bft(self, starting_vertex):
+    """
+    Print each vertex in breadth-first order
+    beginning from starting_vertex.
+    """
+    # Create an empty queue and enqueue the starting_vertex 
+    queue = Queue()
+    queue.enqueue({ 
+        'current_vertex': starting_vertex,
+        'path': [starting_vertex]        
+    })
+    # Create an empty set to track visited verticies
+    visited_vertices = set()
+
+    # while the queue is not empty:
+    while queue.size() > 0:
+        # get current vertex (dequeue from queue)
+        # get current vertex PATH (dequeue from queue)
+        current_obj = queue.dequeue()
+        current_path = current_obj['path']
+        # set the current vertex to the LAST element of the PATH
+        current_vertex = current_obj['current_vertex']
+        # Check if the current vertex has not been visited:
+        if current_vertex not in visited_vertices:
+            # print the current vertex
+            print(current_vertex)
+            # Mark the current vertex as visited
+            # Add the current vertex to a visited_set
+            visited_vertices.add(current_vertex)
+
+            # queue up all the current vertex's neighbors (so we can visit them next)
+            for neighbor_vertex in self.get_neighbors(current_vertex):
+                new_path = list(current_path)
+                new_path.append(neighbor_vertex)
+
+                queue.enqueue({
+                    'current_vertex': neighbor_vertex,
+                    'path': new_path
+                })
+    return None
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -51,12 +178,12 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
